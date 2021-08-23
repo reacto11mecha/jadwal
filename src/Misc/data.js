48d2c0ee0096ab845851c3acd5efaeba616ba331
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-08-23 07:54:40 +0700

    Menambahkan data jadwal pelajaran

---
+++ src/Misc/data.js
@@ -0,0 +1,37 @@
+const jadwal = [
+  {
+    hari: "Senin",
+    mapel: [null, "Seni Budaya", "Bahasa Inggris", "Antropologi"],
+  },
+  {
+    hari: "Selasa",
+    mapel: ["BK4", "Bahasa Jepang", "Matematika", "Pendidikan Agama Islam"],
+  },
+  {
+    hari: "Rabu",
+    mapel: [
+      "Ekonomi",
+      "Bahasa dan Sastra Inggris",
+      "Bahasa Sunda",
+      "Bahasa Indonesia",
+    ],
+  },
+  {
+    hari: "Kamis",
+    mapel: ["PKWU", "Biologi", "PPKN", "Sejarah Indonesia"],
+  },
+  {
+    hari: "Jum'at",
+    mapel: [null, "PJOK", "Informatika", "Bahasa dan Sastra Indonesia"],
+  },
+];
+
+const longest = jadwal
+  .map(({ mapel }) => mapel.length)
+  .sort((a, b) => b - a)[0];
+const newArray = Array.from(new Array(longest));
+
+export default jadwal;
+export const columnRemap = newArray.map((_, idx) =>
+  jadwal.map(({ mapel }) => mapel[idx])
+);
