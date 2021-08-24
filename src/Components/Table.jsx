a8c48e84c66aa1fa4c7fc6fc87011730cf3c42e6
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-08-24 19:02:04 +0700

    Menambahkan fitur highlight untuk hari belajar selanjutnya dan mengubah warna background

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,44 +1,49 @@
-import { createMemo, createSignal } from "solid-js";
-
 import styles from "./Table.module.css";
 import data, { columnRemap } from "../Misc/data";
 
-function Table() {
-  const currentDayIndex = createMemo(() => new Date().getDay());
-
+function Table({ currentDayIndex, nextDay }) {
   return (
-    <div className={styles.scrollableHorizontal}>
-      <table className={`primary ${styles.table}`}>
-        <thead>
-          <tr>
-            <For each={data.map(({ hari }) => hari)}>
-              {(item) => <th className={styles.center}>{item}</th>}
+    <>
+      <div>
+        <h1 className={styles.center}>Jadwal Pelajaran Kelas X Bahasa</h1>
+      </div>
+      <div className={styles.scrollableHorizontal}>
+        <table className={`primary ${styles.table}`}>
+          <thead>
+            <tr>
+              <For each={data.map(({ hari }) => hari)}>
+                {(item) => <th className={styles.center}>{item}</th>}
+              </For>
+            </tr>
+          </thead>
+          <tbody>
+            <For each={data.map((_, idx) => idx)}>
+              {(index) => (
+                <tr className={styles.center}>
+                  <For each={columnRemap[index]}>
+                    {(mataPelajaran) => (
+                      <td
+                        className={
+                          mataPelajaran.index === currentDayIndex()
+                            ? styles.highlighted
+                            : mataPelajaran.index === nextDay()
+                            ? styles.highlightedNextDay
+                            : ""
+                        }
+                      >
+                        {mataPelajaran.mapel === null
+                          ? ""
+                          : mataPelajaran.mapel}
+                      </td>
+                    )}
+                  </For>
+                </tr>
+              )}
             </For>
-          </tr>
-        </thead>
-        <tbody>
-          <For each={data.map((_, idx) => idx)}>
-            {(index) => (
-              <tr className={styles.center}>
-                <For each={columnRemap[index]}>
-                  {(mataPelajaran) => (
-                    <td
-                      className={
-                        mataPelajaran.index === currentDayIndex()
-                          ? styles.highlighted
-                          : ""
-                      }
-                    >
-                      {mataPelajaran.mapel === null ? "" : mataPelajaran.mapel}
-                    </td>
-                  )}
-                </For>
-              </tr>
-            )}
-          </For>
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
 
