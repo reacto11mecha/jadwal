7028982b3aa8a5967cb8afb6771250c5703ade3d
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-08-24 19:01:00 +0700

    Memindahkan currentDayIndex ke App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
+import { createMemo } from "solid-js";
+
 import styles from "./App.module.css";
 
 import Table from "./Components/Table";
+import NextDay from "./Components/NextDay";
 
 function App() {
+  const currentDayIndex = createMemo(() => new Date().getDay());
+  const nextDay = createMemo(() => {
+    const today = currentDayIndex();
+
+    if (today > 4 || today < 1) return 1;
+    return today + 1;
+  });
+
   return (
     <div className={`flex one ${styles.App}`}>
-      <div>
-        <h1>Jadwal Pelajaran Kelas X Bahasa</h1>
-      </div>
-      <Table />
+      <Table currentDayIndex={currentDayIndex} nextDay={nextDay} />
+      <NextDay nextDay={nextDay} />
     </div>
   );
 }
