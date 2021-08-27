c0e0dbc0468893c602c84690b79bfd80d5ba72fd
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-08-27 07:32:50 +0700

    Memperbaiki next day ketika ada jadwal yang null dan undefined

--- src/Components/NextDay.jsx
+++ src/Components/NextDay.jsx
@@ -4,14 +4,19 @@ import data from "../Misc/data";
 import styles from "./NextDay.module.css";
 
 function NextDay({ nextDay }) {
-  const next = createMemo(() => data[nextDay() - 1]);
+  const next = createMemo(() =>
+    data[nextDay() - 1].mapel
+      .filter((x) => x !== undefined)
+      .filter((x) => x !== null)
+  );
+
   return (
     <div className={styles.nextDay}>
       <h3>Mata Pelajaran Selanjutnya</h3>
       <p>
         Hari Belajar Selanjutnya adalah hari <b>{next().hari}</b>
       </p>
-      <p>Mapel: {next().mapel.join(", ")}.</p>
+      <p>Mapel: {next().join(", ")}.</p>
     </div>
   );
 }
--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,50 +1,46 @@
 import styles from "./Table.module.css";
 import data, { columnRemap } from "../Misc/data";
 
-function Table({ currentDayIndex, nextDay }) {
-  return (
-    <>
-      <div>
-        <h1 className={styles.center}>Jadwal Pelajaran Kelas X Bahasa</h1>
-      </div>
-      <div className={styles.scrollableHorizontal}>
-        <table className={`primary ${styles.table}`}>
-          <thead>
-            <tr>
-              <For each={data.map(({ hari }) => hari)}>
-                {(item) => <th className={styles.center}>{item}</th>}
-              </For>
-            </tr>
-          </thead>
-          <tbody>
-            <For each={data.map((_, idx) => idx)}>
-              {(index) => (
-                <tr className={styles.center}>
-                  <For each={columnRemap[index]}>
-                    {(mataPelajaran) => (
-                      <td
-                        className={
-                          mataPelajaran.index === currentDayIndex()
-                            ? styles.highlighted
-                            : mataPelajaran.index === nextDay()
-                            ? styles.highlightedNextDay
-                            : ""
-                        }
-                      >
-                        {mataPelajaran.mapel === null
-                          ? ""
-                          : mataPelajaran.mapel}
-                      </td>
-                    )}
-                  </For>
-                </tr>
-              )}
+const Table = ({ currentDayIndex, nextDay }) => (
+  <>
+    <div>
+      <h1 className={styles.center}>Jadwal Pelajaran Kelas X Bahasa</h1>
+    </div>
+    <div className={styles.scrollableHorizontal}>
+      <table className={`primary ${styles.table}`}>
+        <thead>
+          <tr>
+            <For each={data.map(({ hari }) => hari)}>
+              {(item) => <th className={styles.center}>{item}</th>}
             </For>
-          </tbody>
-        </table>
-      </div>
-    </>
-  );
-}
+          </tr>
+        </thead>
+        <tbody>
+          <For each={data.map((_, idx) => idx)}>
+            {(index) => (
+              <tr className={styles.center}>
+                <For each={columnRemap[index]}>
+                  {(mataPelajaran) => (
+                    <td
+                      className={
+                        mataPelajaran.index === currentDayIndex()
+                          ? styles.highlighted
+                          : mataPelajaran.index === nextDay()
+                          ? styles.highlightedNextDay
+                          : ""
+                      }
+                    >
+                      {mataPelajaran.mapel === null ? "" : mataPelajaran.mapel}
+                    </td>
+                  )}
+                </For>
+              </tr>
+            )}
+          </For>
+        </tbody>
+      </table>
+    </div>
+  </>
+);
 
 export default Table;
